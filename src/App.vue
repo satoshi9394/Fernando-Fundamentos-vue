e3447a8d0b841c1a8c607ec21d8dfb23de6fc305
Angel Santillan <angelsb94@gmail.com>
2021-08-08 18:04:32 -0500

    termin la seccion 4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter title="Entraron" :start="150"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from '@/components/Counter';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,61 @@
+<template>
+  <h2>{{ title }}</h2>
+  <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      //required: true,
+      default: 100,
+      validator( value ) {
+        return value > 100
+      }
+    }
+  },
+  data() {
+    return {
+      counter: this.start
+    };
+  },
+  methods: {
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
+    }
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    }
+  }
+
+}
+</script>
+
+<style>
+  button {
+    background-color: #64bb87;
+    border-radius: 5px;
+    color: red;
+    cursor: pointer;
+    margin: 5px;
+    padding: 5px 15px;
+    transition: 0.3px ease-in-out;
+  }
+  button:hover {
+    background-color: #5aa67b;
+    transition: 0.3 ease-in-out;
+  }
+
+</style>
