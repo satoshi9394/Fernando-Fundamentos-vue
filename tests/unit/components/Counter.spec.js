7e8a8537b9d3afc1d7356aaad0dc5b50b62f5ea9
Angel Santillan <angelsb94@gmail.com>
2021-08-18 16:31:02 -0500

    clases del counter test

--- tests/unit/components/Counter.spec.js
+++ tests/unit/components/Counter.spec.js
@@ -3,37 +3,54 @@ import Counter from '@/components/Counter';
 
 
 describe('Counter component', () => {
-  /*
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Counter);
+  });
+
   test('debe de hacer match con el snapshot', () => {
-    const wrapper = shallowMount( Counter );
+    wrapper = shallowMount( Counter );
     expect(wrapper.html()).toMatchSnapshot();
   });
-  */
+
   test('H2 debe tener el valor por defecto Counter', () => {
-    const wrapper = shallowMount(Counter);
     const h2 = wrapper.find('h2');
     expect(h2.exists()).toBeTruthy();
     expect(h2.text()).toBe('Counter');
   });
 
   test('El valor por defecto debe ser 100 en el p', () => {
-    const wrapper = shallowMount(Counter);
     const secondP = wrapper.find('[data-testid="counter"]');
     expect(secondP.exists()).toBeTruthy();
     expect(secondP.text()).toBe('100');
   });
 
   test('debe de incrementar y decrementar el contador', async() => {
-    const wrapper = shallowMount(Counter);
-    const buttonIncrement = wrapper.find('button');
+    const [buttonIncrement, btnDecrease] = wrapper.findAll('button');
     await buttonIncrement.trigger('click');
-    let value = wrapper.find('[data-testid="counter"]').text();
+    await buttonIncrement.trigger('click');
+    await buttonIncrement.trigger('click');
+    await btnDecrease.trigger('click');
+    await btnDecrease.trigger('click');
+
+    const value = wrapper.find('[data-testid="counter"]').text();
     expect(value).toBe('101');
-    const btnDecrease = wrapper.findAll('button')[1];
-    await btnDecrease.trigger('click');
-    await btnDecrease.trigger('click');
-    value = wrapper.find('[data-testid="counter"]').text();
-    expect(value).toBe('99');
+  });
+
+  test('debe de establecer el valor por defecto', () => {
+    const { start } = wrapper.props();
+    const value = wrapper.find('[data-testid="counter"]').text();
+    expect( Number(value)).toBe(start)
+  })
+
+  test('debe de de mostrar el titulo enviado por props', () => {
+    const titleProps = 'Hola mundo'
+    const wrapperProps = shallowMount(Counter, {
+      props: {
+        title: titleProps
+      }
+    });
+    expect( wrapperProps.find('h2').text()).toBe(titleProps)
   });
 
 });
